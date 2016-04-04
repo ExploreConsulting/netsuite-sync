d6fb922c9b9630b62be1d0f950a113db27e292d7
shawn talbert <ironside14@gmail.com>
2016-04-04 08:57:30 -0700

    Initial commit from private repo.

---
+++ ns.js
@@ -0,0 +1,146 @@
+#!/usr/bin/env node
+
+// this is the console program
+
+var fileCabinet  = require("./lib/FileCabinet");
+var program      = require("commander");
+var chalk        = require('chalk');
+var secureConfig = require("./lib/SecureConfigFile");
+var fs           = require('fs');
+var path         = require('path');
+var _            = require('lodash');
+// this is one of the few libs I found that is compatible with the (linux) webstorm terminal
+var readlineSync = require('readline-sync');
+// custom debugger that logs only if environment variable DEBUG=ns
+var debug = require('debug')('ns');
+
+const CONFIG_FILE = 'NetSuiteConfig.js';
+
+
+// configure the command line interface
+program
+    .version(require('./package.json').version)
+    .option('-u, --upload <file>', "Upload file to NetSuite file cabinet")
+    .option('-d, --desc description', "Description for uploaded file")
+    .option('-e, --encrypt-config', "encrypts the config file using the NSPW environment variable (must" +
+    " be set prior) as passphrase")
+    .option('--decrypt-config', "decrypts the config file and displays the plaintext")
+    .option('-c, --create-config', "displays a sample generic configuration which you save as " + CONFIG_FILE  +
+    " then fill out and run the encrypt (-e) command")
+    .option('-g, --gen-config', "Contacts NetSuite for config information and generates a config file so you don't " +
+    "have to populate the config file entirely by hand")
+    .on('--help', function () {
+        console.log('Examples:');
+        console.log();
+        console.log('Generate (unencrypted) config file interactively:')
+        console.log(chalk.inverse(' ns -g '))
+        console.log();
+        console.log('Upload a file to the folder set in the config file:')
+        console.log(chalk.inverse(' ns -u EC_UserEvent.js '))
+        console.log();
+        console.log('run in debug mode (linux/osx)')
+        console.log(chalk.inverse(' DEBUG=ns; ns -u EC_UserEvent.js '))
+    })
+    .parse(process.argv);
+
+if (program.decryptConfig) {
+    var plaintext = secureConfig.decryptFile(CONFIG_FILE + ".enc");
+    console.log(plaintext);
+    process.exit();
+}
+
+
+if (program.encryptConfig) {
+    // only just prior to encrypt should the NetSuiteConfig.js file be cleartext
+    var out = secureConfig.encryptFile(CONFIG_FILE);
+    console.log("wrote file:" + out);
+    process.exit();
+}
+
+if (program.upload) {
+    fileCabinet.postFile(program.upload, program.desc, function (err, resp) {
+
+        if (err) throw err;
+
+        var wr = resp.Envelope.Body.addResponse.writeResponse;
+        if (wr.status.isSuccess == "true") {
+            var successMsg = "File uploaded successfully as internalid " + wr.baseRef.internalId;
+            console.log(chalk.green(successMsg));
+        }
+        else {
+            var failMsg = "Problem uploading file" + JSON.stringify(wr);
+            console.error(chalk.red(failMsg));
+        }
+    });
+}
+
+if (program.createConfig) {
+    createConfig().then(console.log)
+}
+
+/**
+ * creates a config file string, optionally data binding it
+ * @param {{account, email, password, role, webserviceshost, folderid}} params data elements to inject into the template
+ * @returns {Promise} promise to return the entire config file string
+ */
+function createConfig(params) {
+    var configTemplate = path.join(__dirname, "lib/NetSuiteConfigTemplate.js");
+    return new Promise(function (resolve, reject) {
+        fs.readFile(configTemplate, function (err, template) {
+            if (err) reject(err);
+            else {
+                if (params) template = _.template(template)(params);
+                resolve(template.toString());
+            }
+        });
+    });
+}
+
+if (program.genConfig) {
+    console.log("Generating " + CONFIG_FILE + "...")
+    console.log('Enter credentials to select account/role to use..')
+    var username = readlineSync.question('Account login email:');
+    var password = readlineSync.question('Account login password:');
+    console.log('Enter the internal id of the folder to which files will be saved. If you do not set this it will' +
+        ' default to zero and you must edit the config file manually to set the folder id value');
+    var folder = readlineSync.question('Destination Folder Id:');
+    var isSandbox = readlineSync.keyInYN('Sandbox Account?');
+
+    fileCabinet.discoverConfigInfo(username, password,isSandbox)
+        .then(function (result) {
+            debug('Received body %s', result.body);
+            var accountInfo = promptUserForAccountSelection(JSON.parse(result.body));
+            if (!accountInfo) process.exit();
+            debug('user selected %s', JSON.stringify(accountInfo, null, "  "));
+            return createConfig({
+                account: accountInfo.account.internalId,
+                email: username,
+                password: password,
+                role: accountInfo.role.internalId,
+                webserviceshost: accountInfo.dataCenterURLs.webservicesDomain,
+                folderid: folder || 0
+            })
+        })
+        .then(function(configData) {
+            fs.writeFileSync(CONFIG_FILE, configData);
+            console.log('wrote ' + CONFIG_FILE)
+            var out = secureConfig.encryptFile(CONFIG_FILE);
+            console.log("wrote " + out);
+            console.log("don't forget to delete " + CONFIG_FILE + " after you've tested it's working!")
+        })
+        .catch(console.error)
+}
+
+/**
+ * prompts the user to select an account+role
+ * @param {Array.<{account, role}>} info JSON as returned from NS 'roles' api
+ * @returns {{account,role,dataCenterURLs}} the account info object selected
+ */
+function promptUserForAccountSelection(info) {
+    // create <Account Name> (Role Name) labels for the questions
+    var questions = _.map(info, function (r) {
+        return r.account.name + ' (' + r.role.name + ')'
+    });
+    var index     = readlineSync.keyInSelect(questions, 'Which NetSuite Account (Role) to use?')
+    return info[index];
+}
---
+++ readme.md
@@ -0,0 +1,123 @@
+NS - NetSuite Simple Sync utility
+==============================
+This little utility uploads your suitescript files to the NetSuite file cabinet from the command line or as a
+ menu item or button in your IDE.
+
+The idea is it's much faster fhan to fiddling with the netsuite UI to repeatedly upload a code file you're developing.
+
+# Installing
+
+To install globally
+
+    npm install -g suitescript-file-sync
+
+The commandline program is named `ns`
+
+Get help with `ns --help`
+
+# Getting Started
+This utility uploads files to a specific folder in your NetSuite file cabinet. We've found it a best practice to keep
+all suitescripts in a single folder if possible.
+
+### Windows Users
+We recommend you install Git Bash and run all this stuff in the bit bash shell.
+
+1. Login to your NetSuite account and navigate to the folder you want to upload to - note the internal id of the folder.
+2. Set a password as an in-memory environment variable - this is used to encrypt the generated config file
+
+
+    export NSPW="mypassword"
+
+3. Generate a config file with the `-g` or `--gen-config` option
+
+
+    ns -g
+
+Answer the prompts and supply the internal id of the destination folder you noted in step 1. This will generate both
+the encrypted and unencrypted NetSuiteConfig file.
+
+4. Upload a file
+
+
+    ns -u SomeScriptFile.js
+
+5. Confirm the file is there in NetSuite, then delete the plaintext NetSuiteConfig.js file.
+
+# More Detail
+Although generating a config with the `--gen-config` option is the easiest way to go, you can do it more
+manually with steps described in this section.
+
+## Set your password
+The following sets your password to 'foo'. Note you need to run this prior to running the utility. For an example
+of creating a script to launch WebStorm see here [todo:add link]()
+
+    export NSPW="foo"
+
+## Create NetSuite Config
+This tool uses SuiteTalk (NetSuite web services) to send files to the file cabinet. Hence it needs some configuration
+connection info stored locally. This connection info is stored in a file named *NetSuiteConfig.js* which is encrypted
+to *NetSuiteConfig.js.enc*
+
+To generate a config file run with the `--gen-config` option:
+
+    ns -g
+
+Answer the prompts. You can review the settings in the generated NetSuiteConfig.js file. Do remember to delete this file
+after you've confirmed it works (e.g. after you've successfully uploaded a file). Only NetSuiteConfig.js.enc is needed at
+runtime.
+
+You can manually encrypt a NetSuiteConfig.js file with the `--encrypt` option.
+
+Fill out your plaintext NetSuiteConfig.js file, then encrypt it:
+
+    ns --encrypt-config
+
+Then delete your plaintext NetSuiteConfig.js
+
+If you ever need to check the config, use --decrypt-config:
+
+    ns --decrypt-config
+
+To help setup a new config file:
+
+    ns --geturl
+
+## Upload a suitescript file to NS
+
+    ns -u filename
+or
+
+
+    ns --upload filename
+
+
+...pushes __filename__ to the NS file cabinet under the folder specified in _NetSuiteConfig.js_
+
+
+#### Development/Tech notes
+
+First step: `npm install` to pull in all dependencies.
+
+
+
+##### Debugging
+
+The webstorm regular node debug session doesn't seem to support interactive programs. So, launch the program in a 
+separate terminal window like:
+
+    node --debug-brk=5858 ./ns.js 
+    
+Then create a node _remote debug_ session using this port and run it.
+
+To just spew debug messages to the console, define an environment variable before launching like:
+
+    DEBUG=ns node ./ns.js
+
+##### XML files
+XML files under `/lib` named \<operation\>Template.xml are SOAP snippets representing the NetSuite web service _operation_
+
+For example, _getTemplate.xml_ is the **get** operation as described in the NetSuite [wsdl](https://webservices.netsuite.com/wsdl/v2014_2_0/netsuite.wsdl).
+
+
+
+
